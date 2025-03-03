1f438aadd3914154c95a0e157b83b91988c29bca
David <tigerstone3112@gmail.com>
2025-03-03 19:30:35 +1300

    Fix server import path for ES modules

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { createServer } from 'http'
-import { app } from '../server'
+import { app } from '../server/index.js'
 
 const server = createServer(app)
 
